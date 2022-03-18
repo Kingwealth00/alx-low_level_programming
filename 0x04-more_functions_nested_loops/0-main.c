2c839d61ff4162ebb0058ce4ee007d0c5f473b54
Kingwealth00 <oniteniolapamilerin@gmail.com>
2022-03-18 13:02:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return Always return 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
